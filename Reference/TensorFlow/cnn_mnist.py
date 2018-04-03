c8dcea43c9091c8271ae0efe58e643526711b347
david coxon <davidcoxon@davidcoxon.com>
2018-04-03 05:18:28 +0100

    update tensorflow files
    update tensorflow files

---
+++ Reference/TensorFlow/cnn_mnist.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Imports
+import numpy as np
+import tensorflow as tf
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+# Our application logic will be added here
+
+if __name__ == "__main__":
+  tf.app.run()
